4fabc8276b90f16d9ed34275e9188fd2d0e1261e
LAURO GEOVANNY GUNCAY TIGRE <lgguncay@utpl.edu.ec>
2024-01-20 17:30:48 -0500

    home and items ui

--- lib/screens/home/components/body.dart
+++ lib/screens/home/components/body.dart
@@ -1,26 +1,52 @@
 import 'package:flutter/material.dart';
 import 'package:sigv/constants.dart';
+import '../../../models/Product.dart';
+import '../../details/details.dart';
+import 'item_card.dart';
 
 
 class Body extends StatelessWidget {
   const Body({super.key});
 
+
   @override
   Widget build(BuildContext context) {
     return Column(
       crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
+      children: <Widget>[
         Padding(
           padding: const EdgeInsets.symmetric(horizontal: kDefaultPaddin),
           child: Text(
-            "Women",
+            "Products",
             style: Theme.of(context)
                 .textTheme
                 .headlineSmall
                 ?.copyWith(fontWeight: FontWeight.bold),
           ),
         ),
-
+        Expanded(
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: kDefaultPaddin),
+            child: GridView.builder(
+                itemCount: products.length,
+                gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 2,
+                  mainAxisSpacing: kDefaultPaddin,
+                  crossAxisSpacing: kDefaultPaddin,
+                  childAspectRatio: 0.75,
+                ),
+                itemBuilder: (context, index) => ItemCard(
+                  product: products[index],
+                  press: () => Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => DetailsScreen(
+                          product: products[index],
+                        ),
+                      )),
+                )),
+          ),
+        ),
       ],
     );
   }
--- lib/screens/home/home_screen.dart
+++ lib/screens/home/home_screen.dart
@@ -1,22 +1,23 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
 import 'package:sigv/constants.dart';
-
 import 'components/body.dart';
 
 class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: buildAppBar(),
-      body: Body(),
+      body: const Body(),
     );
   }
 
   AppBar buildAppBar() {
     return AppBar(
-      backgroundColor: Colors.yellow,
-      elevation: 0,
+      backgroundColor: Colors.white,
+      elevation: 1,
       leading: IconButton(
         icon: ColorFiltered(
           colorFilter: const ColorFilter.mode(kTextColor, BlendMode.srcIn),
